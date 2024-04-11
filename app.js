9a43788c42533272196113279ac05f94e141a58c
Mardinik <marcos.dnieto@alumnos.upm.es>
2024-04-11 18:51:30 +0200

    Agregamos Proyecto

---
+++ app.js
@@ -0,0 +1,75 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var flash = require('connect-flash'); //p
+const passport = require("./passport/setup"); //p
+const session = require("express-session");  //p
+const MongoStore = require("connect-mongo")(session); //p
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login'); 
+const signupRouter = require('./routes/signup'); 
+
+var app = express();
+
+//database setup
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost:27017/starwars';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Express Session
+app.use(   
+    session({
+        secret: "very secret this is",
+        resave: false,
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection })
+    })
+);
+
+// Passport middleware
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+app.use(flash()); 
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/signup', signupRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+module.exports = app;
+
---
+++ routes/index.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var router = express.Router();
+var o2x = require('object-to-xml');
+var mongoose = require('mongoose');
+var Personaje = require('../models/personaje'); 
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  try {
+    const personajes = await Personaje.find({});
+    res.render('index', { title: 'Starwars page', personajes, user: req.user });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Está registrado?
+function isLoggedIn (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+// Ruta GET para las páginas de personaje
+router.get('/character/:_id', isLoggedIn, async function(req, res, next) {
+  try {
+    var characterID = req.params._id;
+    var personaje = await Personaje.findById(characterID).exec();
+    if (personaje) {
+      res.render('personaje', { title: 'Página de personaje', personaje: personaje });
+    } else {
+      var err = new Error('Character Not Found');
+      err.status = 404;
+      next(err);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Crear personaje
+router.post('/', async function(req, res, next) { 
+  try {
+    var nombre = req.body.nombre; 
+    var fuerza = req.body.fuerza; 
+    var faccion = req.body.faccion; 
+
+    var nuevoPersonaje = new Personaje({
+      Nombre: nombre,
+      Fuerza: fuerza,
+      Faccion: faccion
+    });
+
+    await nuevoPersonaje.save();
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Eliminar personaje
+router.post('/character/:ID', async function(req, res, next) { 
+  try {
+    var characterID = req.params.ID;
+    await Personaje.findOneAndDelete({_id: characterID});
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Ruta GET para XML
+router.get('/xml', async (req, res, next) => {
+  var personajes = await Personaje.find();
+    //Pasando a string y luego de nuevo a JSON nos evitamos errores de conversion posteriores con o2x
+    personajesFixed = JSON.parse(JSON.stringify(personajes));
+    res.set('Content-Type', 'text/xml');
+    res.send(o2x({
+      '?xml version="1.0" encoding="utf-8"?': null, personajes: { "personaje": personajesFixed }
+    }));
+});
+
+// Ruta GET para JSON
+router.get('/json', async function(req, res, next) {
+  try {
+    const personajes = await Personaje.find({});
+    res.set('Content-Type', 'application/json');
+    res.json(personajes);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
+
